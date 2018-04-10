94220626371bdc38792576f0a4c66c4a9d647643
Sythrill <ksciwiarska@gmail.com>
2018-04-10 19:28:47 +0200

    remove nested switch

--- app/index.js
+++ app/index.js
@@ -1,24 +1,32 @@
 var OSinfo = require('../modules/OSinfo');
 
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function(){
+process.stdin.on('readable', function () {
     var input = process.stdin.read();
-    if(input !== null){
+    if (input !== null) {
         var instruction = input.trim();
-        switch(instruction){
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            case '/getOSinfo':
-                OSinfo.print();
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        }
     }
-});
+    switch (instruction) {
+        case "":
+        case null:
+        case undefined:
+            console.log('Usage:');
+            console.log('/sayhello -- print welcome message');
+            console.log('/getOSinfo -- print info about your OS system');
+            console.log('/exit -- close appilcation');
+            break;
+        case '/exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+        case '/sayhello':
+            process.stdout.write('hello!\n');
+            break;
+        case '/getOSinfo':
+            OSinfo.print();
+            break;
+        default:
+            process.stderr.write('Wrong instruction!\n');
+    }
 
+});
