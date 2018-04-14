4524c532a2ad566c6f2d5f6f83833d3e7c865c42
Sythrill <ksciwiarska@gmail.com>
2018-04-14 15:44:45 +0200

    change upload function and server.js code

--- fileServer/modules/handlers.js
+++ fileServer/modules/handlers.js
@@ -1,35 +1,65 @@
 var fs = require('fs');
 var formidable = require('formidable');
 var path = require('path');
+var imgDir = 'C:/Pliki/';
+var url = require('url');
 
 
 exports.upload = function (request, response) {
     console.log('Rozpoczynam obsługę żądania upload.');
+
     var form = new formidable.IncomingForm();
+
     form.parse(request, function (error, fields, files) {
+
         var oldpath = files.upload.path;
         var filename = files.upload.name;
-        var newpath = 'C:/Pliki/' + filename;
+        var newpath = imgDir + filename;
 
         fs.rename(oldpath, newpath, function (err) {
             if (err) throw err;
-            var xxx = fs.readdirSync('C:/Pliki/');
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.write('recived image:<br/>');
-            for (var i in xxx) {
-                response.write("<img src='/show' />");
-                response.end();
-            }
-        });
-    });
 
+            console.log('Plik załadowany na serwer');
+
+            var files = fs.readdirSync(imgDir);
+
+            response.writeHead(200, {'Content-type':'text/html'});
+            response.write('Pliki wgrane na serwer:<br/ ');
+            var imgList = '<ul>';
+            for (var i = 0; i < files.length; i ++) {
+                imgList += '<li><a href="/?show=' + files[i] + '">' + files[i] + '</li>';
+            }
+            imgList += '</ul>';
+            response.end(imgList);
+        });
+
+    });
 };
 
 exports.welcome = function (request, response) {
+
     console.log('Rozpoczynam obsługę żądania welcome.');
+
     fs.readFile('templates/start.html', function (err, html) {
         response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
         response.write(html);
+        response.end();
+    });
+};
+
+exports.show = function (request, response) {
+
+    var query = url.parse(request.url, true).query;
+
+    //console.log(query);
+
+    pic = query.show;
+
+    //console.log(pic);
+
+    fs.readFile(pic, "binary", function (error, file) {
+        response.writeHead(200, {"Content-Type":"image/png"});
+        response.write(file, "binary");
         response.end();
     });
 };
@@ -39,17 +69,6 @@ exports.error = function (request, response) {
     response.write('404 :(');
     response.end();
 };
-
-exports.show = function (request, response, filename) {
-    fs.readFile('C:/Pliki/' + filename, 'binary', function (error, file) {
-        response.writeHead(200, {'Content-Type':'image/jpg'});
-        response.write(file, 'binary');
-        response.end();
-    });
-};
-
-
-
 
 
 
--- fileServer/modules/server.js
+++ fileServer/modules/server.js
@@ -1,26 +1,31 @@
 var http = require('http');
 var colors = require('colors');
 var handlers = require('./handlers');
+var url = require('url');
 
 function start() {
     function onRequest(request, response) {
+
         console.log("Odebrano zapytanie.");
         console.log('Zapytanie ' + request.url + ' odebrane.');
 
-        response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-        switch(request.url) {
+        var query = url.parse(request.url, true).query;
+        pic = query.show;
+
+        response.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+        switch (request.url) {
             case '/':
             case '/start':
-                handlers.welcome(request,response);
+                handlers.welcome(request, response);
                 break;
             case '/upload':
-                handlers.upload(request,response);
+                handlers.upload(request, response);
                 break;
-            case '/show':
-                handlers.show(request,response);
+            case '/?show=' + pic:
+                handlers.show(request, response);
                 break;
             default:
-                handlers.error(request,response);
+                handlers.error(request, response);
         }
     }
 
@@ -31,5 +36,5 @@ function start() {
 
 
 module.exports = {
-    start: start
+    start:start
 };
